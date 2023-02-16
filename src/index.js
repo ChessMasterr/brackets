7f0bf704f9fa46e4a9544c66efb7d83232697786
ChessMasterr <artiom.farhutdinoff@yandex.ru>
2023-02-16 21:14:59 +0500

    did it

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
-}
+  let a = str;
+  for (let i = 0; i < str.length; i++) {
+    a = a
+      .replaceAll("[]", "")
+      .replaceAll("()", "")
+      .replaceAll("{}", "")
+      .replaceAll("||", "")
+      .replaceAll("12", "")
+      .replaceAll("34", "")
+      .replaceAll("56", "")
+      .replaceAll("77", "")
+      .replaceAll("88", "");
+  }
+
+  return a.length === 0 ? true : false;
+};
